d4b0b9b6ee1f9e0e62ae894279f4f07ee1fe9134
Nguyen Long Phi Hai <nguyenlongphihai@luvina.net>
2024-02-28 16:28:05 +0700

    [bank-service] #validation show error on response

--- bank-service/src/main/kotlin/com/phihai91/bankservice/adapter/in/web/exception/RestResponseEntityExceptionHandler.kt
+++ bank-service/src/main/kotlin/com/phihai91/bankservice/adapter/in/web/exception/RestResponseEntityExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.phihai91.bankservice.adapter.`in`.web.exception
 
 import com.phihai91.bankservice.adapter.`in`.web.dto.CommonErrorResponse
 import jakarta.persistence.EntityNotFoundException
+import jakarta.validation.ConstraintViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -10,10 +11,15 @@ import reactor.core.publisher.Mono
 
 @ControllerAdvice
 class RestResponseEntityExceptionHandler {
-
     @ExceptionHandler
-    fun handleNotFound(ex: EntityNotFoundException) : Mono<ResponseEntity<CommonErrorResponse>> {
+    fun handleNotFound(ex: EntityNotFoundException): Mono<ResponseEntity<CommonErrorResponse>> {
         val res = ex.message?.let { CommonErrorResponse(it) }
         return Mono.just(ResponseEntity<CommonErrorResponse>(res, HttpStatus.NOT_FOUND))
+    }
+
+    @ExceptionHandler
+    fun handleValidation(ex: ConstraintViolationException): Mono<ResponseEntity<CommonErrorResponse>> {
+        val res = ex.message?.let { CommonErrorResponse(it) }
+        return Mono.just(ResponseEntity<CommonErrorResponse>(res, HttpStatus.BAD_REQUEST))
     }
 }
